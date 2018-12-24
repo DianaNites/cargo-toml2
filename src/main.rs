28e835e3cadd77e0727c479f1fe0e354ed35f169
Diana <5275194+DianaNites@users.noreply.github.com>
2018-12-24 00:48:38 -0500

    Extend dev testcase

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,9 @@ fn main() {
     let toml = std::fs::read_to_string("Cargo.toml").expect("Failed to read Cargo.toml");
     let toml: CargoToml = toml::from_str(&toml).expect("Failed to deserialize Cargo.toml");
     println!("{:#?}", toml);
+    std::fs::write(
+        "Test.toml",
+        toml::to_string(&toml).expect("Failed to serialize CargoToml"),
+    )
+    .expect("Failed to write CargoToml");
 }
