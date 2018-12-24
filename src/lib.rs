f688afa25b4c5067f0f2f50aac333feb6591d367
Diana <5275194+DianaNites@users.noreply.github.com>
2018-12-23 21:19:47 -0500

    WIP basic reading

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,42 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use serde_derive::{Deserialize, Serialize};
+
+fn default_edition() -> String {
+    "2015".into()
+}
+
+#[derive(Deserialize, Debug, Serialize)]
+#[serde(untagged)]
+pub enum StringOrBool {
+    String(String),
+    Bool(bool),
+}
+
+impl Default for StringOrBool {
+    fn default() -> Self {
+        StringOrBool::String("build.rs".into())
     }
+}
+
+/// The root Cargo.toml
+#[derive(Deserialize, Debug, Serialize, Default)]
+pub struct CargoToml {
+    pub package: Package,
+}
+
+#[derive(Deserialize, Debug, Serialize, Default)]
+pub struct Package {
+    pub name: String,
+    pub version: String,
+    pub authors: Vec<String>,
+    #[serde(default = "default_edition")]
+    pub edition: String,
+    #[serde(default)]
+    pub build: StringOrBool,
+    pub links: Option<String>,
+    // TODO: Real default is docs.rs link with the current version
+    pub documentation: Option<String>,
+    // TODO: Real default is seeded with VCS ignore, eg .gitignore
+    pub exclude: Option<Vec<String>>,
+    // FIXME: Mutually exclusive with exclude
+    pub include: Option<Vec<String>>,
 }
