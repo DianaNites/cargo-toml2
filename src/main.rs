e2bb509b2328ec87d7dcec195e99c11825266b93
Diana <5275194+DianaNites@users.noreply.github.com>
2018-12-24 15:18:14 -0500

    dev tests

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,13 @@ use cargo_toml2::*;
 fn manifest_test() {
     let toml: CargoToml = from_path("Cargo.toml").expect("Failed to deserialize Cargo.toml");
     println!("{:#?}", toml);
-    std::fs::write(
-        "Test.toml",
-        toml::to_string(&toml).expect("Failed to serialize CargoToml"),
-    )
-    .expect("Failed to write CargoToml");
+    to_path("Test.toml", toml).expect("Failed to serialize/write CargoToml");
 }
 
 fn config_test() {
     let toml: CargoConfig = from_path(".cargo/config").expect("Failed to deserialize CargoConfig");
     println!("{:#?}", toml);
-    std::fs::write(
-        "Test.toml",
-        toml::to_string(&toml).expect("Failed to serialize CargoConfig"),
-    )
-    .expect("Failed to write CargoConfig");
+    to_path("Test.toml", toml).expect("Failed to serialize/write CargoConfig");
 }
 
 fn main() {
